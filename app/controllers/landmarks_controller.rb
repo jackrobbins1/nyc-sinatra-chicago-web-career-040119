a334c6f6f68b13b7c6a81c85c753aee383c6a62d
Jackson Robbins <jackrobbins1@gmail.com>
2019-04-24 14:28:39 -0500

    completed the landmark controller and views

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,43 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  #-------- All
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+#----------------------- New/Show
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
+  end
+
+  post '/landmarks' do
+    landmark = Landmark.create(params[:landmark])
+    redirect "/landmarks/#{landmark.id}"
+  end
+#-------------------- Edit
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/edit"
+  end
+
+  patch '/landmarks/:id' do
+    landmark = Landmark.find(params[:id])
+    landmark.update(params[:landmark])
+    redirect "/landmarks/#{landmark.id}"
+  end
+
+  #----------------Delete
+
+  # delete '/landmarks/:id' do
+  #   landmark = Landmark.find(params[:id])
+  #   landmark.destroy
+  #   redirect "/landmarks"
+  # end
+
+
 end
